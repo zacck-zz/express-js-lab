5209f167e521b4a62eb3339b9fd5fb935b7c3078
zacck <coderv63@gmail.com>
2017-03-27 12:59:54 +0200

    work on app to serve a folder

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var express = require('express');
-
 var app = express();
+var path = require('path');
 
 
 //this takes a route and a functio that takes in a request and response
-app.get('/',(req, res) => {
-  res.send('hello express')
-});
+// app.get('/',(req, res) => {
+//   res.send('hello express')
+// });
+//
+// //add another route
+// app.get('/about', (req, res) => {
+//   res.send('About us')
+// });
 
-//add another route
-app.get('/about', (req, res) => {
-  res.send('About us')
-})
+//lets expose a folder as a webserver
+app.use(express.static(__dirname  + '/public'));
 
 //listen give the app an port to listenm to
 app.listen(3000);
